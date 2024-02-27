c78f0df00f68dc700aebb71e12102ece29825680
Owen Walpole <owen@walpole.dev>
2024-02-27 01:22:54 -0600

    Make root password echo bold and blink

--- board/raspberrypi0w/post-build.sh
+++ board/raspberrypi0w/post-build.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 # Set a random root password
 declare password
 password=$(pwgen --ambiguous)
+tput bold; tput blink
 echo "Your root password is: $password"
+tput sgr0
 sed -i "s|root::|root:$(openssl passwd -5 -noverify -stdin <<<$password):|" "$TARGET_DIR/etc/shadow"
 unset password
